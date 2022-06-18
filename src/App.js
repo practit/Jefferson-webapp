dc249b1b3cc9c90258be477e69f89ad848079c02
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-18 16:45:29 -0300

    Enclosed into a component the WIP page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from './jefferson_logo.png';
-import react_logo from './react_logo.svg';
 import './App.css';
+import WIP from './pages/WIP.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Trabajo en proceso...
-        </h1>
-        <p className='con-amors'>Con ❤ y <img src={react_logo} className="App-logo-pequenio" alt="react logo" />
-        </p>
-      </header>
+      <WIP />
     </div>
   );
 }
