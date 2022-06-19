19960669103cb9cc34d4aec84f32d83b8636b5c9
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-19 19:37:47 +0000

    Created basic pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import WIP from './pages/WIP.js'
-import Usuarios from './pages/Usuarios.js'
-import Sidebar from './components/sidebar'
 import { useState } from 'react';
+import './App.css';
+import Sidebar from './components/sidebar';
+import Estadistica from './pages/Estadistica';
+import Informacion from './pages/Informacion';
+import Inicio from './pages/Inicio';
+import Usuarios from './pages/Usuarios';
+import WIP from './pages/WIP.js';
 function App() {
   const [editor, setEditor] = useState(<WIP />)
   function selectEditor(editor) {
     switch (editor) {
+      case "Inicio":
+        setEditor(<Inicio />)
+        break;
+      case "Estadistica":
+        setEditor(<Estadistica />)
+        break;
+      case "Informacion":
+        setEditor(<Informacion />)
+        break;
       case "Usuarios":
         setEditor(<Usuarios />)
         break;
