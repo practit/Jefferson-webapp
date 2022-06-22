06bb216934bf57228d83275bafb19bf8dd25185c
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-21 22:28:36 -0300

    Fixed html for error from react

--- src/pages/Usuarios.js
+++ src/pages/Usuarios.js
@@ -3,9 +3,9 @@ function Usuarios(){
     <div>
       <h1>Iniciar Sesión</h1>
       <form className="loginForm">
-        <label for="Usuario">Usuario: </label>
+        <label htmlFor="Usuario">Usuario: </label>
         <input id="Usuario" type="text"/>
-        <label for="Contrasenia">Contraseña: </label>
+        <label htmlFor="Contrasenia">Contraseña: </label>
         <input id="Contrasenia" type="password" />
         <br />
         <input type="submit" className="formBtn" />
